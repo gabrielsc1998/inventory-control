b0324dfc208f1b23249df3c143ada97aa3341133
gabriel.caetano <98gabrielsc@gmail.com>
2022-09-30 12:20:09 -0300

    feat(application): implements helpers
    Implements http response helpers

---
+++ backend/src/application/helpers/http-helper.ts
@@ -0,0 +1,23 @@
+import { HttpResponse } from "@/application/contracts/http";
+
+import { UnauthorizedError } from "../errors";
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data,
+});
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error,
+});
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error,
+});
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError(),
+});
