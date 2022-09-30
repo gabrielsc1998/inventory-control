d302159bf96a6903498c98ce53523dffb010b8b7
gabriel.caetano <98gabrielsc@gmail.com>
2022-09-30 14:03:24 -0300

    feat(application): implements auth controller
    Implements refresh-token controller

---
+++ backend/src/application/controllers/auth/refresh-token/index.ts
@@ -0,0 +1,34 @@
+import * as response from "@/application/helpers";
+import { InvalidTokenError } from "@/domain/errors";
+import { HttpResponse } from "@/application/contracts/http";
+import { hasAllFields } from "@/application/validators/fields";
+import { Controller } from "@/application/contracts/controllers";
+import { UserRefreshToken } from "@/domain/modules/user/use-cases";
+
+export class RefreshTokenController implements Controller {
+  constructor(private readonly refreshTokenUseCase: UserRefreshToken) {}
+  async handle(request: {
+    body: { access_token: string };
+  }): Promise<HttpResponse> {
+    const dtoRequest = {
+      ...(request?.body || {}),
+    };
+
+    const expectedFields = ["access_token"];
+    const fieldError = hasAllFields({ expectedFields, dto: dtoRequest });
+
+    if (fieldError) {
+      return response.badRequest(new Error(`${fieldError} not provided`));
+    }
+
+    const output = await this.refreshTokenUseCase.execute({
+      refreshToken: dtoRequest.access_token,
+    });
+
+    if (output instanceof InvalidTokenError) {
+      return response.unauthorized();
+    }
+
+    return response.ok(output);
+  }
+}
---
+++ backend/src/application/controllers/auth/refresh-token/test/index.spec.ts
@@ -0,0 +1,91 @@
+import {
+  tokenHandlerMock,
+  TokenHandlerMock,
+} from "@/common/tests/mocks/infra/gateways/token-handler";
+import { TokenHandler } from "@/domain/contracts/gateways";
+import { Controller } from "@/application/contracts/controllers";
+import { badRequest, ok, unauthorized } from "@/application/helpers";
+import { UserRefreshTokenUseCase } from "@/application/modules/user/use-cases/refresh-token";
+
+import { RefreshTokenController } from "..";
+
+type SUT = {
+  refreshTokenController: Controller;
+  tokenHandler: TokenHandler;
+};
+
+const makeSut = (): SUT => {
+  const tokenHandler = new TokenHandlerMock();
+  const refreshTokenUseCase = new UserRefreshTokenUseCase(tokenHandler);
+  const refreshTokenController = new RefreshTokenController(
+    refreshTokenUseCase
+  );
+
+  return {
+    refreshTokenController,
+    tokenHandler,
+  };
+};
+
+let sut: SUT = null;
+
+describe("Refresh Token [ Controller ]", () => {
+  beforeAll(() => (sut = makeSut()));
+  beforeEach(() => jest.clearAllMocks());
+
+  it("should return a new tokens", async () => {
+    const token = "refresh-token";
+    const input = {
+      body: {
+        access_token: token,
+      },
+    };
+
+    const spyTokenValidate = jest.spyOn(sut.tokenHandler, "validate");
+    const spyTokenGenerate = jest.spyOn(sut.tokenHandler, "generate");
+
+    const output = await sut.refreshTokenController.handle(input);
+
+    expect(output).toMatchObject(
+      ok({
+        token: tokenHandlerMock.generatedToken,
+        refreshToken: tokenHandlerMock.generatedToken,
+      })
+    );
+    expect(spyTokenValidate).toBeCalledWith({ token });
+    expect(spyTokenGenerate).toBeCalledTimes(2);
+  });
+
+  it("should return error when the input is null", async () => {
+    const spyTokenValidate = jest.spyOn(sut.tokenHandler, "validate");
+    const spyTokenGenerate = jest.spyOn(sut.tokenHandler, "generate");
+
+    const output = await sut.refreshTokenController.handle(null);
+
+    expect(output).toMatchObject(
+      badRequest(new Error(`access_token not provided`))
+    );
+    expect(spyTokenValidate).toBeCalledTimes(0);
+    expect(spyTokenGenerate).toBeCalledTimes(0);
+  });
+
+  it("should return error when the token is invalid", async () => {
+    const token = "invalid-refresh-token";
+    const input = {
+      body: {
+        access_token: token,
+      },
+    };
+
+    const spyTokenValidate = jest
+      .spyOn(sut.tokenHandler, "validate")
+      .mockReturnValue(false);
+    const spyTokenGenerate = jest.spyOn(sut.tokenHandler, "generate");
+
+    const output = await sut.refreshTokenController.handle(input);
+
+    expect(output).toMatchObject(unauthorized());
+    expect(spyTokenValidate).toBeCalledWith({ token });
+    expect(spyTokenGenerate).toBeCalledTimes(0);
+  });
+});
