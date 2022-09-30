64fa3a805c8b5773ea7878c94f24cfea9c335541
gabriel.caetano <98gabrielsc@gmail.com>
2022-09-30 20:09:05 -0300

    feat: add auth middleware to routes

--- backend/src/main/application/routes/app/categories/index.ts
+++ backend/src/main/application/routes/app/categories/index.ts
@@ -3,17 +3,20 @@ import {
   makeListCategoriesController,
 } from "@/main/application/controllers/categories";
 import { Routes } from "@/application/contracts/routes";
+import { makeAuthMiddleware } from "@/main/application/middlewares/auth";
 
 const categoryRoutes: Routes = {
   list: {
     method: "GET",
     path: "/categories",
     controller: makeListCategoriesController(),
+    middleware: makeAuthMiddleware(),
   },
   create: {
     method: "POST",
     path: "/categories",
     controller: makeCreateCategoryController(),
+    middleware: makeAuthMiddleware(),
   },
 };
 
--- backend/src/main/application/routes/app/products/index.ts
+++ backend/src/main/application/routes/app/products/index.ts
@@ -4,22 +4,26 @@ import {
   makeListProductsController,
 } from "@/main/application/controllers/products";
 import { Routes } from "@/application/contracts/routes";
+import { makeAuthMiddleware } from "@/main/application/middlewares/auth";
 
 const productRoutes: Routes = {
   list: {
     method: "GET",
     path: "/products",
     controller: makeListProductsController(),
+    middleware: makeAuthMiddleware(),
   },
   getById: {
     method: "GET",
     path: "/products:id",
     controller: makeGetOneProductController(),
+    middleware: makeAuthMiddleware(),
   },
   create: {
     method: "POST",
     path: "/products",
     controller: makeCreateProductController(),
+    middleware: makeAuthMiddleware(),
   },
 };
 
