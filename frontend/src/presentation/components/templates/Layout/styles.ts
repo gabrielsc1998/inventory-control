4ab9518904ead3399f0b2d4cfe26a10ee0e43147
gabriel.caetano <98gabrielsc@gmail.com>
2022-10-05 09:14:02 -0300

    chore: adjust layout style

--- frontend/src/presentation/components/templates/Layout/styles.ts
+++ frontend/src/presentation/components/templates/Layout/styles.ts
@@ -3,8 +3,8 @@ import styled, { css } from "styled-components";
 export const Container = styled.div`
   ${({ theme }) => css`
     width: 100%;
-    height: 100vh;
-    min-height: 100%;
+    height: 100%;
+    min-height: 100vh;
 
     background: ${theme.colors.ui.primary};
 
