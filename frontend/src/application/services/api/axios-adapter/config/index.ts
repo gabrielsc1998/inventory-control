9e079ff66133ee2679c1784f44d6ae8a2800e0a9
gabriel.caetano <98gabrielsc@gmail.com>
2022-10-05 10:33:05 -0300

    feat: implements the get url from env

--- frontend/src/application/services/api/axios-adapter/config/index.ts
+++ frontend/src/application/services/api/axios-adapter/config/index.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from "axios";
 
 export const AXIOS_CONFIG: AxiosRequestConfig = {
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 15000,
 };
