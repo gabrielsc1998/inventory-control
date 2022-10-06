51a0773fc785f08325c20010a1dd4e55499b8813
gabriel.caetano <98gabrielsc@gmail.com>
2022-10-06 15:35:23 -0300

    feat: implements ssr in product page
    Implements redirect when the token not exists in domain storage

--- frontend/src/pages/products/index.tsx
+++ frontend/src/pages/products/index.tsx
@@ -1,5 +1,9 @@
+import { GetServerSideProps, GetServerSidePropsContext, Redirect } from "next";
+
+import { SCREEN_ROUTES } from "presentation/routes";
 import ProductsScreen from "presentation/screens/Products";
 import Layout from "presentation/components/templates/Layout";
+import { makeDomainStorageGateway } from "main/infra/gateways/domain-storage";
 
 const ProductsPage = (): JSX.Element => {
   return (
@@ -10,3 +14,28 @@ const ProductsPage = (): JSX.Element => {
 };
 
 export default ProductsPage;
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+): Promise<
+  | {
+      props: {};
+    }
+  | {
+      redirect: Redirect;
+    }
+> => {
+  const domainStorage = makeDomainStorageGateway();
+  const token = domainStorage.get({ key: "token", ctx: ctx });
+  if (!token) {
+    return {
+      redirect: {
+        permanent: true,
+        destination: SCREEN_ROUTES.LOGIN,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+};
